0ba8111b47bc03b81ec1ea6ee41396ea92f8bad0
Robert Malko <robmalko@gmail.com>
2009-08-21 01:23:18 -0400

    little bit of a hack, but I'm accessing the MongoDB Interactive Shell through node and can pipe processes through it.  Just for POC I'm able to output my local databases.  You'll need to set where your mongo is in line 1 of mongo.js

---
+++ examples/01_show_databases.js
@@ -0,0 +1,9 @@
+var mongo = require("mongo.js");
+
+function runDemo() {
+  mongo.showDatabases();
+}
+
+function onLoad() {
+  setTimeout(runDemo, 1000);
+}
--- mongo.js
+++ mongo.js
@@ -1,74 +1,12 @@
-// mongo.js
-// NOTE 'Mongo' may be defined here or in MongoJS.cpp. Add code to init, not to this constructor.
-if (typeof Mongo == "undefined") {
-    Mongo = function(host) {
-        this.init(host);
-    }
-}
+var conn = node.createProcess("/mongodb/bin/mongo");
+var CRLF = "\n";
+var callbacks = [];
 
-Mongo.prototype.find = function(ns, query, fields, limit, skip) {
-    throw "find not implemented";
-}
-Mongo.prototype.insert = function(ns, obj) {
-    throw "insert not implemented";
-}
-Mongo.prototype.remove = function(ns, pattern) {
-    throw "remove not implemented;"
-}
-Mongo.prototype.update = function(ns, query, obj) {
-    throw "update not implemented;"
-}
+conn.addListener("output", function (data) {
+  puts(data);
+});
 
-mongoInject(Mongo.prototype);
-
-Mongo.prototype.setSlaveOk = function() {
-    this.slaveOk = true;
-}
-
-Mongo.prototype.getDB = function(name) {
-    return new DB(this, name);
-}
-
-Mongo.prototype.getDBs = function() {
-    var res = this.getDB("admin").runCommand({
-        "listDatabases": 1
-    });
-    assert(res.ok == 1, "listDatabases failed");
-    return res;
-}
-
-Mongo.prototype.getDBNames = function() {
-    return this.getDBs().databases.map(
-    function(z) {
-        return z.name;
-    }
-    );
-}
-
-Mongo.prototype.toString = function() {
-    return "mongo connection";
-}
-
-connect = function(url, user, pass) {
-    print("connecting to: " + url)
-
-    if (user && !pass)
-    throw "you specified a user and not a password. either you need a password, or you're using the old connect api";
-
-    var idx = url.indexOf("/");
-
-    var db;
-
-    if (idx < 0)
-    db = new Mongo().getDB(url);
-    else
-    db = new Mongo(url.substring(0, idx)).getDB(url.substring(idx + 1));
-
-    if (user && pass) {
-        if (!db.auth(user, pass)) {
-            throw "couldn't login";
-        }
-    }
-
-    return db;
+this.showDatabases = function() {
+  var cmd = "show dbs" + CRLF;
+  conn.write(cmd);
 }
